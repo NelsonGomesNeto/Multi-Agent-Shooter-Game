a54596af41305a42b7776dacf07d4dcb3a47e4f9
NelsonGomesNeto <ngn@ic.ufal.br>
2019-02-19 06:52:43 -0300

    Adds PathFinder

--- Person/Person.js
+++ Person/Person.js
@@ -1,20 +1,26 @@
 class Person {
   constructor(teamID) {
-    this.position = [];
-    if (teamID == 1) 
-      this.position = createVector(random(width / 6, 2 * width / 6), random(height / 8, 7 * height / 8));
-    else
-      this.position = createVector(random(width - 2 * width / 6, width - width / 6), random(height / 8, 7 * height / 8));
+    // this.position = [];
+    // if (teamID == 1) 
+      this.position = createVector(random(width / 6, 5 * width / 6), random(height / 8, 7 * height / 8));
+      console.log(this.position);
+    // else
+      // this.position = createVector(random(width - 2 * width / 6, width - width / 6), random(height / 8, 7 * height / 8));
     this.velocity = createVector(0, 0);
     this.acceleration = createVector(0, 0);
     this.angle = 0;
     this.topSpeed = 2;
-    this.size = 20;
+    this.size = personSize;
     this.activated = false;
     this.id = idCounter ++;
     this.teamID = teamID;
     this.bullets = new Array();
     this.fireCounter = -1;
+    this.pathID = 1;
+    this.path = false;
+    this.timestamp = 0;
+    this.target = new Position(-1, -1);
+    this.fuck = false;
     /* Extended classes must define:
       color, damage, health, fullHealth, cooldown, bulletWillHit (function)
     */
@@ -56,10 +62,32 @@ class Person {
       this.velocity.y = this.acceleration.y = 0;
   }
 
+  followPath() {
+    let i = int(this.position.y / columnSize), j = int(this.position.x / lineSize);
+    if (!this.fuck && (!this.path || this.pathID >= this.path.length)) {
+      this.path = new PathFinder(this.position.copy(), createVector(random(0, width - 100), random(0, height - 100))).findPath();
+      this.pathID = -1;
+      this.fuck = true;
+      fill(255, 255, 255);
+      // console.log(this.path[0]);
+      // console.log(this.path[this.path.length - 1]);
+      console.log(this.path);
+    } else if (this.pathID >= 0 && (i != this.target.i || j != this.target.j)) {
+      this.position.set(this.target.j * lineSize + lineSize / 2, this.target.i * columnSize + columnSize / 2);
+    } else if (this.path && (this.timestamp ++) == followPathRate && this.pathID < this.path.length - 1) {
+      this.pathID ++;
+      this.target.i = this.path[this.pathID].i, this.target.j = this.path[this.pathID].j;
+      console.log("was: " + this.position.x + " " + this.position.y + " -- " + (this.target.j) * columnSize + " " + (this.target.i * lineSize))
+      this.timestamp = 0;
+      // console.log(i + " " + j + " || " + this.path[this.pathID].i + " " + this.path[this.pathID].j);
+    }
+  }
+
   update() {
     if (this.fireCounter % this.cooldown == 0) this.fireCounter = 0;
     else this.fireCounter ++;
-    this.getMovements();
+    if (this.teamID == 1) this.getMovements();
+    else this.followPath();
     for (var i = 0; i < this.bullets.length; i++)
       this.bullets[i].update();
     this.velocity.add(this.acceleration);
@@ -78,10 +106,10 @@ class Person {
                0 + this.size, 0,
                0, 0 + this.size);
       fill(0, 255, 0);
-      rect(-this.size, -20, 10, 40);
+      rect(-this.size, -this.size, this.size / 2, 2 * this.size);
       fill(255, 0, 0);
       let healthPorcentage = (this.fullHealth - this.health) / this.fullHealth;
-      rect(-this.size, 40 / 2 - (40 * healthPorcentage), 10, 40 * healthPorcentage);
+      rect(-this.size, this.size - (2 * this.size * healthPorcentage), this.size / 2, 2 * this.size * healthPorcentage);
       fill(255, 255, 255); textSize(this.size);
       rotate(Math.PI / 2); text(this.teamID, -this.size / 3, 0);
     pop();
--- game.js
+++ game.js
@@ -1,23 +1,30 @@
-let maxHealth = 100;
+let maxHealth = 100, personSize = 15;
+let lines = 50, columns = 50;
+let followPathRate = 20;
+var lineSize, columnSize;
 var people;
 var idCounter = 0;
 
 function setup() {
-  createCanvas(1000, 600);
+  createCanvas(801, 801);
+  lineSize = ((width - 1) / lines), columnSize = ((height - 1) / columns);
   people = new Array();
   for (var j = 1; j <= 2; j ++)
-    for (var i = 0; i < 2; i ++) {
+    for (var i = 0; i < 1; i ++) {
       people.push(new Shooter(j));
-      people.push(new Healer(j));
+      // people.push(new Healer(j));
     }
 }
 
 function draw() {
   background(131, 136, 132);
 
+  for (var i = 0; i <= lines; i ++) line(0, i * columnSize, width, i * columnSize);
+  for (var i = 0; i <= columns; i ++) line(i * lineSize, 0, i * lineSize, height);
+
   for (var i = 0; i < people.length; i ++) {
     if (people[i].health <= 0) {
-      people.splice(i--, 1);
+      people.splice(i --, 1);
       continue;
     }
     
